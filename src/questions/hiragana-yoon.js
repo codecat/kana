e395149b0e130f75fab55de52bf2515133f32451
Melissa <melissa@nimble.tools>
2024-05-25 00:02:59 +0200

    Update hiragana-yoon.js

--- src/questions/hiragana-yoon.js
+++ src/questions/hiragana-yoon.js
@@ -1,9 +1,9 @@
 export default [
 	{q:'きゃ', a:'kya'}, {q:'きゅ', a:'kyu'}, {q:'きょ', a:'kyo'},
 	{q:'ぎゃ', a:'gya'}, {q:'ぎゅ', a:'gyu'}, {q:'ぎょ', a:'gyo'},
-	{q:'しゃ', a:'sha'}, {q:'しゅ', a:'shu'}, {q:'しょ', a:'sho'},
+	{q:'しゃ', a:['sha','sya']}, {q:'しゅ', a:['shu','syu']}, {q:'しょ', a:['sho','syo']},
 	{q:'じゃ', a:'ja'}, {q:'じゅ', a:'ju'}, {q:'じょ', a:'jo'},
-	{q:'ちゃ', a:'cha'}, {q:'ちゅ', a:'chu'}, {q:'ちょ', a:'cho'},
+	{q:'ちゃ', a:['cha','cya']}, {q:'ちゅ', a:['chu','cyu']}, {q:'ちょ', a:['cho','cyo']},
 	{q:'にゃ', a:'nya'}, {q:'にゅ', a:'nyu'}, {q:'にょ', a:'nyo'},
 	{q:'ひゃ', a:'hya'}, {q:'ひゅ', a:'hyu'}, {q:'ひょ', a:'hyo'},
 	{q:'びゃ', a:'bya'}, {q:'びゅ', a:'byu'}, {q:'びょ', a:'byo'},
